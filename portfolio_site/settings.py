f2c514cbe3a4e6c7091d7d05d154c3a0d76c667a
Arpitha4 <arpithabhatng@gmail.com>
2025-11-30 13:32:56 +0530

    update

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -27,7 +28,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 ROOT_URLCONF = 'portfolio_site.urls'
 
 TEMPLATES = [
